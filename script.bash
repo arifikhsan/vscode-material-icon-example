b11bd4c55c88a19939c8456e8367224db963de06
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-05 19:45:31 +0700

    add subfolders

--- script.bash
+++ script.bash
@@ -1,6 +1,14 @@
-for d in */; do
+# for d in */; do
+#   cd $d
+#   touch .gitkeep
+#   cd ..
+#   echo "$d"
+# done
+
+for d in */**/; do
+  rootdirectory=$(pwd)
   cd $d
   touch .gitkeep
-  cd ..
-#   echo "$d"
+  cd rootdirectory
+  echo "$d"
 done
